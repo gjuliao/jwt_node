746e67b2bf7977e4780da81151679d2a5af92f97
Giovanni Juliao <gjuliao32@gmail.com>
2023-11-15 19:38:54 -0500

    Added the email exist function

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 4050;
-
-app.get("/", (req, res) => {
-    res.send(`Its working!`)
-});
-
-app.listen(PORT, () => console.log(`server running on ${PORT}`));
