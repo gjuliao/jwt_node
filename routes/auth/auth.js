a82199c5b632a2f5e74dcadb8ddee5b892c5f532
Giovanni Juliao <gjuliao32@gmail.com>
2023-11-15 19:45:50 -0500

    Added try catch user register

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -13,4 +13,49 @@ const registerSchema = Joi.object({
     password: Joi.string().min(3).required(),
 })
 
+router.post("/register", async(req, res) => {
+    // checking if user email is already registered
+    const emailExist = await User.findOne({email: req.body.email});
+
+    if (emailExist) {
+        res.status(400).send('Email already exists');
+        return;
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // adding user
+
+    const user = new User({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        password: hashedPassword,
+    });
+
+    try {
+        // validate user input
+
+        const { error } = await registerSchema.validateAsync(req.body);
+
+        // getting error if exists with object deconstruction
+
+        if(error){
+            res.status(400).send(error.details[0].message);
+            return;
+        } else {
+
+            // add user
+            const saveUser = await user.save();
+            res.status(200).send("user created")
+        }
+    }
+
+    catch (error){
+        res.status(500).send(error);
+    }
+});
+
 module.exports = router;
